e4178ba3e1ba387c0e93d51fa5c30088a4f0e9ac
jmjoseph1 <jm.joseph1@outlook.fr>
2019-12-16 15:18:58 +0100

    Modif pService

--- src/com/gestioncontact/services/PersonneService.java
+++ src/com/gestioncontact/services/PersonneService.java
@@ -3,8 +3,8 @@ package com.gestioncontact.services;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 
-import com.formation.annuaire.entities.Personne;
 import com.gestioncontact.dao.PersonneDao;
+import com.gestioncontact.model.Personne;
 
 @Singleton
 public class PersonneService {
